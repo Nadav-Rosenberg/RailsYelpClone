b8cb58bb6dba7e425563d443fe9146e1d8cf9c19
Richard Lynch <rjlynch86@googlemail.com>
2015-05-05 18:01:20 +0100

    adds ability to update restaurants

--- yelp_clone/app/controllers/restaurants_controller.rb
+++ yelp_clone/app/controllers/restaurants_controller.rb
@@ -14,10 +14,21 @@ class RestaurantsController < ApplicationController
 
   def show 
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
     p @restaurant
-  end  
+    @restaurant.update(restaurant_params)
+    redirect_to '/restaurants'
+  end
 
   def restaurant_params
     params.require(:restaurant).permit(:name)
   end
+
 end
--- yelp_clone/spec/features/restaurants_feature_spec.rb
+++ yelp_clone/spec/features/restaurants_feature_spec.rb
@@ -11,8 +11,13 @@ feature 'restaurants' do
 
   context 'restaurants have been added' do
     before do
-      Restaurant.create(name: 'KFC')
-    end  
+      rest = Restaurant.create(name: 'KFC')
+      p rest
+    end 
+
+    after do
+      p "Restaurant.all:  #{Restaurant.all.inspect}"
+    end 
 
     scenario 'display restaurants' do
       visit '/restaurants'
@@ -42,6 +47,19 @@ feature 'restaurants' do
       expect(page).to have_content 'KFC'
       expect(current_path).to eq "/restaurants/#{kfc.id}"
     end  
+  end
+
+  context 'editing restaurants' do
+    before {Restaurant.create(name: 'KFC')}
+
+    scenario 'let a user edit a restaurant' do
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in 'Name', with: 'Kentucky Fried Chicken'
+      click_button 'Update Restaurant'
+      expect(page).to have_content 'Kentucky Fried Chicken'
+      expect(current_path).to eq '/restaurants'
+    end
   end
 
 end
