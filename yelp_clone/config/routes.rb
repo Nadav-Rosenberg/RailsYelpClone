8e140dfcc84bc2f9f83ad90e5bec1c11cf598340
Richard Lynch <rjlynch86@googlemail.com>
2015-05-06 19:52:33 +0100

    adds facebook login via oauth

--- yelp_clone/config/routes.rb
+++ yelp_clone/config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   end
 
   root to: "restaurants#index"
+
+  # devise_scope :user do
+  #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
