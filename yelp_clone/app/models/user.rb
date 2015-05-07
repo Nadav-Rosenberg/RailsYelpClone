13255cb7714f9620efad335245ab569496dab082
Nadav Rosenberggi <nadav.rosenberg@gmail.com>
2015-05-07 11:52:18 +0100

    adds User ID association to Review

--- yelp_clone/app/models/user.rb
+++ yelp_clone/app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ActiveRecord::Base
+  # has_many :reviewed_restaurants, through: :reviews, source: :restaurant
+  has_many :reviews
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, 
          :omniauthable, :omniauth_providers => [:facebook]
+
+       
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -14,11 +21,10 @@ class User < ActiveRecord::Base
     end
   end
 
-    def self.new_with_session(params, session)
+  def self.new_with_session(params, session)
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
-        p user.email
       end
     end
   end
